13b696555a01ed4987b24515331a51f0c80197a3
RISHABH <rishabhchaurasiya20899@gmail.com>
2022-11-28 20:46:50 +0530

    done

---
+++ meals/lib/utils/Appcolors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Colors.deepPurple;
+}
+
+Color body1() {
+  return const Color.fromRGBO(20, 51, 51, 1);
+}
+
+Color body2() {
+  return const Color.fromRGBO(20, 51, 51, 1);
+}
---
+++ meals/lib/utils/TextStyle.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+
+TextStyle textStyle() {
+  return const TextStyle(
+    fontSize: 24,
+    fontFamily: 'RobotoCondensed',
+    fontWeight: FontWeight.bold,
+  );
+}
