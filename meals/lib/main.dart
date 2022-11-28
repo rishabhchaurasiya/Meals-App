6aea73e8e0d0ca79a6c27c19e0eca62d6e02cf61
RISHABH <rishabhchaurasiya20899@gmail.com>
2022-11-28 20:46:32 +0530

    layout of category screen and  navigation screen

--- meals/lib/main.dart
+++ meals/lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import './category_meals_screen.dart';
+import './categories_screens.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -8,29 +11,29 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'DeliMeals',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.deepPurple,
+        accentColor: Colors.amber,
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        // textTheme: ThemeData.light().textTheme.copyWith(
+        //       bodyText1: TextStyle(
+        //         color: Color.fromRGBO(20, 51, 51, 1),
+        //       ),
+        //       bodyText2: TextStyle(
+        //         color: Color.fromRGBO(20, 51, 51, 1),
+        //       ),
+        //       titleLarge: TextStyle(
+        //         fontSize: 24,
+        //         fontFamily: 'RobotoCondensed',
+        //       ),
+        //     ),
       ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-    @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('DeliMeals'),
-      ),
-      body: Center(
-        child: Text('Navigation Time!'),
-      ),
+      // home: CategoriesScreen(),
+      initialRoute: '/', // default is '/'
+      routes: {
+        '/': (context) => CategoriesScreen(),
+        '/categories-meals': (ctx) => CategoryMealScreen(),
+      },
     );
   }
 }
